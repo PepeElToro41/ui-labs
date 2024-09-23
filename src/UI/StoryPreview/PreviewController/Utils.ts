43a857f338177e4f7f5e0af3758a2b01ec330fd7
PepeElToro41 <da907852@gmail.com>
2024-09-23 04:15:38 -0600

    fix: story unmounting signal wasnt disconnecting

--- src/UI/StoryPreview/PreviewController/Utils.ts
+++ src/UI/StoryPreview/PreviewController/Utils.ts
@@ -1,15 +1,19 @@
 import { Signal } from "@rbxts/lemon-signal";
+import { useMemo } from "@rbxts/react";
 import { StoryBase } from "@rbxts/ui-labs/src/Typing/Typing";
 
 //This hook runs the cleanup function automatically, because I always forget to add it ;-;
 export function useStoryUnmount(story: StoryBase, unmountSignal: Signal, unmounter: () => void) {
-	unmountSignal.Connect(() => {
-		unmounter();
-		if (story.cleanup) {
-			const [success, err] = pcall(story.cleanup);
-			if (!success) {
-				warn("UI Labs: Error while running cleanup function: ", err);
+	useMemo(() => {
+		//using useMemo for running this instantly
+		unmountSignal.Once(() => {
+			unmounter();
+			if (story.cleanup) {
+				const [success, err] = pcall(story.cleanup);
+				if (!success) {
+					warn("UI Labs: Error while running cleanup function: ", err);
+				}
 			}
-		}
-	});
+		});
+	}, []);
 }
