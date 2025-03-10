c8f73cc41ed22e1d824a7111783a09e587a9be7b
PepeElToro41 <da907852@gmail.com>
2025-03-10 04:33:30 -0600

    fix unmount typo

--- src/UI/Overlays/Dropdown/StoryDropdown.tsx
+++ src/UI/Overlays/Dropdown/StoryDropdown.tsx
@@ -1,15 +1,19 @@
 import React from "@rbxts/react";
-import Dropdown from ".";
-import DropdownEntry from "./DropdownEntry";
-import { Counter } from "Utils/NumberUtils";
+import {
+	useProducer,
+	useSelector,
+	useSelectorCreator
+} from "@rbxts/react-reflex";
 import { Selection } from "@rbxts/services";
-import { useProducer, useSelector, useSelectorCreator } from "@rbxts/react-reflex";
-import Divisor from "UI/Utils/Divisor";
-import { selectMountAmount, selectPreview } from "Reflex/StoryPreview";
-import { useOverlayAction } from "../Utils";
 import { usePlugin } from "Hooks/Reflex/Use/Plugin";
 import Configs from "Plugin/Configs";
 import { selectKeepViewOnViewport } from "Reflex/PluginSettings";
+import { selectMountAmount, selectPreview } from "Reflex/StoryPreview";
+import Divisor from "UI/Utils/Divisor";
+import { Counter } from "Utils/NumberUtils";
+import Dropdown from ".";
+import { useOverlayAction } from "../Utils";
+import DropdownEntry from "./DropdownEntry";
 
 interface StoryDropdownProps {
 	Position: UDim2 | React.Binding<UDim2>;
@@ -17,19 +21,25 @@ interface StoryDropdownProps {
 }
 
 function StoryDropdown(props: StoryDropdownProps) {
-	const { mountOnWidget, mountStory, mountOnTop, unmountByModule } = useProducer<RootProducer>();
+	const { mountOnWidget, mountStory, mountOnTop, unmountByModule } =
+		useProducer<RootProducer>();
 	const keepViewOnViewport = useSelector(selectKeepViewOnViewport);
 	const mountAmount = useSelectorCreator(selectMountAmount, props.Node.Module);
 	const rootEntry = useSelectorCreator(selectPreview, Configs.RootPreviewKey);
 
 	const count = Counter();
 	const plugin = usePlugin();
-	const isAlreadyMounted = rootEntry ? rootEntry.Module === props.Node.Module : false;
+	const isAlreadyMounted = rootEntry
+		? rootEntry.Module === props.Node.Module
+		: false;
 
 	const deps = [props.Node, keepViewOnViewport];
 	const module = props.Node.Module;
 
-	const OnViewStory = useOverlayAction(() => mountStory(module, keepViewOnViewport), deps);
+	const OnViewStory = useOverlayAction(
+		() => mountStory(module, keepViewOnViewport),
+		deps
+	);
 	const OnMountOnTop = useOverlayAction(() => mountOnTop(module), deps);
 	const OnMountOnWidget = useOverlayAction(() => mountOnWidget(module), deps);
 
@@ -38,12 +48,35 @@ function StoryDropdown(props: StoryDropdownProps) {
 
 	return (
 		<Dropdown key={"StoryDropdown"} Position={props.Position}>
-			<DropdownEntry Text="Mount Story" Disabled={isAlreadyMounted} OnClick={OnViewStory} LayoutOrder={count()} />
-			<DropdownEntry Text="Mount On Top" OnClick={OnMountOnTop} LayoutOrder={count()} />
-			<DropdownEntry Text="Mount In Widget" Disabled={plugin === undefined} OnClick={OnMountOnWidget} LayoutOrder={count()} />
+			<DropdownEntry
+				Text="Mount Story"
+				Disabled={isAlreadyMounted}
+				OnClick={OnViewStory}
+				LayoutOrder={count()}
+			/>
+			<DropdownEntry
+				Text="Mount On Top"
+				OnClick={OnMountOnTop}
+				LayoutOrder={count()}
+			/>
+			<DropdownEntry
+				Text="Mount In Widget"
+				Disabled={plugin === undefined}
+				OnClick={OnMountOnWidget}
+				LayoutOrder={count()}
+			/>
 			<Divisor Order={count()} />
-			<DropdownEntry Text="Ummount All" Disabled={!(mountAmount > 0)} OnClick={OnUnmountAll} LayoutOrder={count()} />
-			<DropdownEntry Text="Select Module" OnClick={OnSelectModule} LayoutOrder={count()} />
+			<DropdownEntry
+				Text="Unmount All"
+				Disabled={!(mountAmount > 0)}
+				OnClick={OnUnmountAll}
+				LayoutOrder={count()}
+			/>
+			<DropdownEntry
+				Text="Select Module"
+				OnClick={OnSelectModule}
+				LayoutOrder={count()}
+			/>
 		</Dropdown>
 	);
 }
