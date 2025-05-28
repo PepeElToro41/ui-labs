a6fcfe9951754825f37e20fa42a600d3963d1fcc
PepeElToro41 <da907852@gmail.com>
2025-05-27 19:19:23 -0600

    make docs changes

--- docs/.vitepress/config/shared.ts
+++ docs/.vitepress/config/shared.ts
@@ -1,22 +1,35 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 export const shared = defineConfig({
 	title: "UI Labs",
 	titleTemplate: ":title - UI Labs",
-    base: "/ui-labs/",
-    cleanUrls: true,
+	base: "/ui-labs/",
+	cleanUrls: true,
 
-    rewrites: {
-        'en/:rest*': ':rest*'
-    },
+	rewrites: {
+		"en/:rest*": ":rest*"
+	},
 
-    head: [
-		["link", { rel: "icon", type: "image/x-icon", href: "/ui-labs/home/pluginicon.ico" }],
-		["link", { rel: "preconnect", href: "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@600;700&display=swap" }],
+	head: [
+		[
+			"link",
+			{
+				rel: "icon",
+				type: "image/x-icon",
+				href: "/ui-labs/home/pluginicon.ico"
+			}
+		],
+		[
+			"link",
+			{
+				rel: "preconnect",
+				href: "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@600;700&display=swap"
+			}
+		]
 	],
 
-    themeConfig: {
-        logo: "/home/pluginicon.ico",
-        socialLinks: [{ icon: "github", link: "https://github.com/PepeElToro41/ui-labs" }],
-    }
-})
+	themeConfig: {
+		logo: "/home/pluginicon.ico",
+		socialLinks: [{ icon: "github", link: "https://ui-labs.luau.page" }]
+	}
+});
